a468bf5894ebdc727bbc574b27a027b2f4a8acad
bmuhamood <bbosa2009@gmail.com>
2021-02-09 13:02:38 -0800

    added classes and constructors for the Book

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 // Book Class: Prepresents a Books
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
 
 // UI Class: Handle UI Tasks
 
+class UI {
+    static displayBooks(){
+        const StoredBooks = [
+            {
+                title: "book one",
+                author: "Muha",
+                isbn: "12345"
+            },
+
+            {
+                title: "bbosa",
+                author: "mb",
+                isbn: "23453"
+            }
+        ];
+
+        const books = StoredBooks;
+        
+        books.forEach((book) => UI.addBookToList(book));
+    }
+    static addBookToList(book) {
+        const list  = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${title}</td>
+            <td>${author}</td>
+            <td>${isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</td>
+        `;
+
+        list.appendChild(row);
+    }
+}
+
 // Store Class: Handles Storage
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
 
